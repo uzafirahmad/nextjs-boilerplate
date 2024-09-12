f943f686f1cac4983b8349a76265ae157df166fb
Uzafir Ahmad <84019473+uzafirahmad@users.noreply.github.com>
2024-09-12 13:10:07 +0500

    Update page.js

--- app/forgot-password/page.js
+++ app/forgot-password/page.js
@@ -5,50 +5,50 @@ import Form from './components/Form'
 import BackButton from './components/BackButton'
 
 export const metadata = {
-    title: "Forgot Password - Ordion",
-    description: "Recover your password for Ordion. Follow the steps to reset your password and regain access to your account.",
-    keywords: "forgot password, reset password, recover password, Ordion password recovery",
-    alternates: {
-        canonical: "https://ordion.io/forgot-password",
-    },
-    robots: "index, follow",
-    viewport: "width=device-width, initial-scale=1",
-    charset: "UTF-8",
-    language: "en",
-    author: "Ordion Team",
-    publisher: "Ordion Inc.",
-    applicationName: "Ordion",
-    twitter: {
-        card: 'summary_large_image',
-        title: 'Forgot Password - Ordion',
-        description: "Recover your password for Ordion. Follow the steps to reset your password and regain access to your account.",
-        images: [
-            {
-                url: 'https://ordion.io/seo_banner.jpg',
-                alt: 'Ordion Forgot Password Seo Banner',
-                width: 1200,
-                height: 630,
-            }
-        ],
-        locale: 'en_US',
-        type: 'website',
-    },
-    openGraph: {
-        title: 'Forgot Password - Ordion',
-        description: "Recover your password for Ordion. Follow the steps to reset your password and regain access to your account.",
-        url: 'https://ordion.io/forgot-password',
-        type: 'website',
-        locale: 'en_US',
-        images: [
-            {
-                url: 'https://ordion.io/seo_banner.jpg',
-                alt: 'Ordion Forgot Password Seo Banner',
-                width: 1200,
-                height: 630,
-            }
-        ],
-        site_name: 'Ordion',
-    },
+  title: "",
+  description: "",
+  keywords: "",
+  alternates: {
+    canonical: "",
+  },
+  robots: "index, follow",
+  viewport: "width=device-width, initial-scale=1",
+  charset: "UTF-8",
+  language: "en",
+  author: "",
+  publisher: "",
+  applicationName: "",
+  twitter: {
+    card: 'summary_large_image',
+    title: '',
+    description: "",
+    images: [
+      {
+        url: '',
+        alt: '',
+        width: 1200,
+        height: 630,
+      }
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  openGraph: {
+    title: '',
+    description: "",
+    url: '',
+    type: 'website',
+    locale: 'en_US',
+    images: [
+      {
+        url: '',
+        alt: '',
+        width: 1200,
+        height: 630,
+      }
+    ],
+    site_name: '',
+  },
 };
 
 
